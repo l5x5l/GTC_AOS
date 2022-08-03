b450579e36d43185b066af89af849301ade47330
yunsehwan <sh10302@naver.com>
2022-08-03 12:18:05 +0900

    feed 내용 보여주는 부분 bindingAdapter 적용

--- app/src/main/java/com/softsquared/gridge_test/android/instagram_challenge/base_component/BindingAdapters.kt
+++ app/src/main/java/com/softsquared/gridge_test/android/instagram_challenge/base_component/BindingAdapters.kt
@@ -1,11 +1,15 @@
 package com.softsquared.gridge_test.android.instagram_challenge.base_component
 
+import android.text.Html
 import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.softsquared.gridge_test.android.instagram_challenge.R
 import com.softsquared.gridge_test.android.instagram_challenge.custom_view.ViewLoginEditText
 import com.softsquared.gridge_test.android.instagram_challenge.data.in_app.StringWrapper
+import com.softsquared.gridge_test.android.instagram_challenge.utils.getTimeDiffFromCurrent
 
 object BindingAdapters {
 
@@ -41,5 +45,17 @@ object BindingAdapters {
     @BindingAdapter("isVisible")
     fun setVisibilityByBoolean(view : View, isVisible : Boolean = false){
         view.visibility = if (isVisible) View.VISIBLE else View.INVISIBLE
+    }
+
+    @JvmStatic
+    @BindingAdapter(value = [ "userId", "content" ])
+    fun setUserAndContent(view : AppCompatTextView, userId : String, context : String) {
+        view.text = Html.fromHtml("<b>$userId</b> $context", Html.FROM_HTML_MODE_LEGACY)
+    }
+
+    @JvmStatic
+    @BindingAdapter("createdAt")
+    fun setCreatedAtString(view : AppCompatTextView, createdAt : String) {
+        view.text = getTimeDiffFromCurrent(createdAt, view.context)
     }
 }
